00bc0c4d8c7c8225a398950f9451fe63cf8cf4f4
Nisk1G <sk23919j@st.omu.ac.jp>
2024-01-05 16:19:04 +0900

    [Add] bus api

--- app/src/main.py
+++ app/src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import json
 
 #Google Sheets Authentication
 Auth = "auth.json" # ここにjsonファイルのPathを入力する
@@ -10,18 +11,27 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(Auth, scope)
 Client = gspread.authorize(credentials)
 
 #Sheet Key
-bus_time_sheet_key = "1-O0RRZyd_xCGoj1cwoHYrigngggZx1g_Yzw0zMPwBDs"
+master_data_sheet_key = "1-O0RRZyd_xCGoj1cwoHYrigngggZx1g_Yzw0zMPwBDs"
 
 app = FastAPI()
 
 @app.get("/")
 def hello_world():
     print("Hello world")
-    return {"message": "Hello World"}
+    return [{"message": "Hello World"}, {"message": "Test"}]
 
 @app.get("/bus")
 def get_bus_time():
-    SpreadSheet = Client.open_by_key(bus_time_sheet_key)
-    RawData = SpreadSheet.worksheet("シート1")
+    #read
+    SpreadSheet = Client.open_by_key(master_data_sheet_key)
+    RawData = SpreadSheet.worksheet("bus_schedule")
     Data = pd.DataFrame(RawData.get_all_values())
+    #process
+    Data.columns = Data.iloc[0]
+    Data = Data[1:]
+    json_data = Data.to_json(orient='records')
+    res = json.loads(json_data)
+    
     print(Data)
+    print(res)
+    return res
