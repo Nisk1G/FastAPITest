e94df070dfe54baa97c5800112ceef7cac9a95ac
Nisk1G <sk23919j@st.omu.ac.jp>
2024-01-05 17:39:25 +0900

    [Add] 2キャンパスの半径500mの飲食店の取得

--- app/src/main.py
+++ app/src/main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
+import googlemaps
+import pprint
 
 #Google Sheets Authentication
 Auth = "auth.json" # ここにjsonファイルのPathを入力する
@@ -10,8 +12,18 @@ scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(Auth, scope)
 Client = gspread.authorize(credentials)
 
+#Google Map Authentication
+map_key = "AIzaSyBuB8FxDFCich3vPRNGuGEhgizio86bDjI"
+map_client = googlemaps.Client(map_key)
+
 #Sheet Key
 master_data_sheet_key = "1-O0RRZyd_xCGoj1cwoHYrigngggZx1g_Yzw0zMPwBDs"
+
+#constraints
+mozu_lat = 34.5450379
+mozu_lng = 135.5066057
+sugi_lat = 34.5906436
+sugi_lng = 135.504993
 
 app = FastAPI()
 
@@ -35,3 +47,44 @@ def get_bus_time():
     print(Data)
     print(res)
     return res
+
+@app.get("/mozu_food")
+def get_mozu_food():
+    loc = {'lat': mozu_lat, 'lng': mozu_lng}
+    place_results = map_client.places_nearby(location=loc, radius=500, keyword='レストラン',language='ja') 
+    pprint.pprint(place_results)
+    results = []
+    photos = []
+    for place_result in place_results['results']:
+        results.append(place_result)
+        if not 'photos' in place_result.keys():
+            photo = 'no image'
+            photos.append(photo)
+        else:
+            p_value = place_result['photos'][0]['photo_reference']
+            photo = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={}&key={}'.format(p_value,map_key)
+            photos.append(photo)
+    pprint.pprint(results)
+    
+    return {'infos': results, 'photos' : photos}
+
+@app.get("/sugi_food")
+def get_sugi_food():
+    loc = {'lat': sugi_lat, 'lng': sugi_lng}
+    place_results = map_client.places_nearby(location=loc, radius=500, keyword='レストラン',language='ja') 
+    pprint.pprint(place_results)
+    results = []
+    photos = []
+    for place_result in place_results['results']:
+        results.append(place_result)
+        if not 'photos' in place_result.keys():
+            photo = 'no image'
+            photos.append(photo)
+        else:
+            p_value = place_result['photos'][0]['photo_reference']
+            photo = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={}&key={}'.format(p_value,map_key)
+            photos.append(photo)
+    pprint.pprint(results)
+    
+    return {'infos': results, 'photos' : photos}
+
