41e03261aa125b504eea7e0b33a3e86ad35be262
Nisk1G <sk23919j@st.omu.ac.jp>
2024-01-10 20:14:26 +0900

    [Update] food api

--- app/src/main.py
+++ app/src/main.py
@@ -5,6 +5,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 import json
 import googlemaps
 import pprint
+import pytz
+from datetime import datetime, time
 
 #Google Sheets Authentication
 auth = "auth.json"
@@ -12,20 +14,44 @@ scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(auth, scope)
 client = gspread.authorize(credentials)
 
-#Google Map Authentication
-map_key = "AIzaSyBuB8FxDFCich3vPRNGuGEhgizio86bDjI"
-map_client = googlemaps.Client(map_key)
-
 #Sheet Key
 master_data_sheet_key = "1-O0RRZyd_xCGoj1cwoHYrigngggZx1g_Yzw0zMPwBDs"
 
-#constraints
-mozu_lat = 34.5450379
-mozu_lng = 135.5066057
-sugi_lat = 34.5906436
-sugi_lng = 135.504993
-
 app = FastAPI()
+
+'''
+input : sheet_name at spreadsheet(https://docs.google.com/spreadsheets/d/1-O0RRZyd_xCGoj1cwoHYrigngggZx1g_Yzw0zMPwBDs/edit#gid=2010775418)
+output : pd.DataFrame format
+'''
+def read_spreadsheet(sheet_name):
+    #read
+    spread_sheet = client.open_by_key(master_data_sheet_key)
+    raw_data = spread_sheet.worksheet(sheet_name)
+    data = pd.DataFrame(raw_data.get_all_values())
+    
+    return data
+
+# string(time)⇒time object
+def parse_time(time_str):
+    if time_str == "0:00":
+        return time(23, 59, 59)
+    return datetime.strptime(time_str, '%H:%M').time()
+
+# open_now value check
+def is_open(row, current_weekday, current_time):
+    if row['regular_holiday'] == current_weekday:
+        return 'FALSE'
+    
+    lunch_open = parse_time(row['lunch_opening_hour'])
+    lunch_close = parse_time(row['lunch_closing_hour'])
+    dinner_open = parse_time(row['dinner_opening_hour'])
+    dinner_close = parse_time(row['dinner_closing_hour'])
+
+    if lunch_open <= current_time <= lunch_close or dinner_open <= current_time <= dinner_close:
+        return 'TRUE'
+    else:
+        return 'FALSE'
+
 
 @app.get("/")
 def hello_world():
@@ -34,60 +60,32 @@ def hello_world():
 
 #bus api
 @app.get("/bus")
-def get_bus_time():
-    #read
-    SpreadSheet = client.open_by_key(master_data_sheet_key)
-    RawData = SpreadSheet.worksheet("bus_schedule")
-    Data = pd.DataFrame(RawData.get_all_values())
+def get_bus_info():
+    data = read_spreadsheet("bus_schedule")
     #process
-    Data.columns = Data.iloc[0]
-    Data = Data[1:]
-    json_data = Data.to_json(orient='records')
+    data.columns = data.iloc[0]
+    data = data[1:]
+    json_data = data.to_json(orient='records')
     res = json.loads(json_data)
-    
-    print(Data)
-    print(res)
     return res
 
-#nakamozu food api
-@app.get("/mozu_food")
-def get_mozu_food():
-    loc = {'lat': mozu_lat, 'lng': mozu_lng}
-    place_results = map_client.places_nearby(location=loc, radius=500, keyword='レストラン',language='ja') 
-    pprint.pprint(place_results)
-    results = []
-    photos = []
-    for place_result in place_results['results']:
-        results.append(place_result)
-        if not 'photos' in place_result.keys():
-            photo = 'no image'
-            photos.append(photo)
-        else:
-            p_value = place_result['photos'][0]['photo_reference']
-            photo = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={}&key={}'.format(p_value,map_key)
-            photos.append(photo)
-    pprint.pprint(results)
-    
-    return {'infos': results, 'photos' : photos}
-
-#sugimoto food api
-@app.get("/sugi_food")
-def get_sugi_food():
-    loc = {'lat': sugi_lat, 'lng': sugi_lng}
-    place_results = map_client.places_nearby(location=loc, radius=500, keyword='レストラン',language='ja') 
-    pprint.pprint(place_results)
-    results = []
-    photos = []
-    for place_result in place_results['results']:
-        results.append(place_result)
-        if not 'photos' in place_result.keys():
-            photo = 'no image'
-            photos.append(photo)
-        else:
-            p_value = place_result['photos'][0]['photo_reference']
-            photo = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={}&key={}'.format(p_value,map_key)
-            photos.append(photo)
-    pprint.pprint(results)
-    
-    return {'infos': results, 'photos' : photos}
+#food api　(campus : string)
+@app.get("/food")
+def get_food_info(campus):
+    data = read_spreadsheet("food_shop")
+    ##process
+    data.columns = data.iloc[0]
+    data = data[1:]
+    #process(campus)
+    campus_string = campus
+    data = data[data['campus'] == campus_string]
+    #process(open_now)
+    jst = pytz.timezone('Asia/Tokyo')
+    now = datetime.now(jst)
+    time = now.time()
+    weekday = now.strftime('%a')
+    data['open_now'] = data.apply(is_open, axis=1, current_weekday = weekday, current_time = time)
+    json_data = data.to_json(orient='records')
+    res = json.loads(json_data)
+    return res
 
